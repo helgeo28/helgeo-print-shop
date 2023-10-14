d4793f3913ecf7d77167345fe7d701163af9fd15
josephmalete1997 <josephmalete75@gmail.com>
2023-10-14 02:27:58 +0200

    Making fixes

--- js/items.js
+++ js/items.js
@@ -1,42 +1,45 @@
 import * as itemLIst from "./content.js";
 
 function getItems(UniformItems, TextItem) {
-    let productID;
-    if (UniformItems === itemLIst.uniform_A) {
-        productID = "a";
-    }
-    if (UniformItems === itemLIst.uniform_B) {
-        productID = "b";
-    }
+  let productID;
+  if (UniformItems === itemLIst.uniform_A) {
+    productID = "a";
+  }
+  if (UniformItems === itemLIst.uniform_B) {
+    productID = "b";
+  }
 
-    if (UniformItems === itemLIst.uniform_C) {
-        productID = "c";
-    }
-    if (UniformItems === itemLIst.uniform_D) {
-        productID = "d";
-    }
-    if (UniformItems === itemLIst.uniform_E) {
-        productID = "e";
-    }
-    if (UniformItems === itemLIst.uniform_F) {
-        productID = "f";
-    }
-    if (UniformItems === itemLIst.uniform_G) {
-        productID = "g";
-    }
-    if (UniformItems === itemLIst.uniform_H) {
-        productID = "h";
-    }
-    if (UniformItems === itemLIst.uniform_I) {
-        productID = "i";
-    }
-    return (
-        UniformItems.forEach((UniformItems, index) => {
-            const borderHolder = document.createElement('div');
-            borderHolder.setAttribute("class", "borderHolder")
-            borderHolder.setAttribute("data-id", `${productID}${index+1}`);
-            const container = document.getElementById("category");
-            borderHolder.innerHTML = `<i class='fa-regular fa-heart'></i>
+  if (UniformItems === itemLIst.uniform_C) {
+    productID = "c";
+  }
+  if (UniformItems === itemLIst.uniform_D) {
+    productID = "d";
+  }
+  if (UniformItems === itemLIst.uniform_E) {
+    productID = "e";
+  }
+  if (UniformItems === itemLIst.uniform_F) {
+    productID = "f";
+  }
+  if (UniformItems === itemLIst.uniform_G) {
+    productID = "g";
+  }
+  if (UniformItems === itemLIst.uniform_H) {
+    productID = "h";
+  }
+  if (UniformItems === itemLIst.uniform_I) {
+    productID = "i";
+  }
+  return UniformItems.forEach((UniformItems, index) => {
+    const borderHolder = document.createElement("div");
+    borderHolder.setAttribute("class", "borderHolder");
+    borderHolder.setAttribute("data-id", `${productID}${index + 1}`);
+    const container = document.getElementById("category");
+    borderHolder.innerHTML = `
+    <div class='unfilled'>
+    <i class='fa-regular fa-heart unfilled-heart'></i>
+    </div>
+    <div class='filled'> <i class="fa-solid fa-heart filled-heart"></i></div>
             <div class="item-description">${TextItem[index]}</div>
             <button class="add-to-cart add"><i class="fa-solid fa-cart-plus"></i> Add item
             <div class="loadersAdd">
@@ -47,13 +50,155 @@ function getItems(UniformItems, TextItem) {
             </div>
             </button>
             `;
-            borderHolder.style.backgroundImage = `url(${UniformItems})`;
-            borderHolder.style.backgroundPositionY = `10px`;
-            // text
-            container.appendChild(borderHolder);
-        })
-    )
+    borderHolder.style.backgroundImage = `url(${UniformItems})`;
+    borderHolder.style.backgroundPositionY = `10px`;
+    // text
+    container.appendChild(borderHolder);
 
+    /*
+    let unfilled = document.querySelectorAll(".unfilled");
+    let filled = document.querySelectorAll(".filled");
+
+    unfilled.forEach((unfilled, index) => {
+      unfilled.style.transition = ".5s";
+      filled[index].style.transition = ".5s";
+      unfilled.addEventListener("click", () => {
+        unfilled.style.transform = "scale(2.5) rotate(360deg)";
+        setTimeout(() => {
+          unfilled.style.display = "none";
+          filled[index].style.display = "block";
+          filled[index].style.transform = "scale(2.5)";
+          setTimeout(() => {
+            filled[index].style.transform = "scale(1) rotate(0deg)";
+          }, 200);
+        }, 500);
+      });
+    });
+
+    filled.forEach((filled, index) => {
+      filled.style.transition = ".5s";
+      unfilled[index].style.transition = ".5s";
+      filled.addEventListener("click", () => {
+        filled.style.transform = "scale(2.5) rotate(360deg)";
+        setTimeout(() => {
+          filled.style.display = "none";
+          unfilled[index].style.display = "block";
+          unfilled[index].style.transform = "scale(2.5)";
+          setTimeout(() => {
+            unfilled[index].style.transform = "scale(1) rotate(0deg)";
+          }, 200);
+        }, 500);
+      });
+    });
+    //end
+    */
+
+    /*
+Start
+*/
+    let unfilled = document.querySelectorAll(".unfilled");
+    let filled = document.querySelectorAll(".filled");
+
+    // Function to save styles to local storage
+    function saveStylesToLocalStorage() {
+      // Create an array to store the styles
+      const stylesArray = [];
+
+      unfilled.forEach((unfilled) => {
+        unfilled.style.transition = ".5s";
+        const styles = {
+          transition: unfilled.style.transition,
+          transform: unfilled.style.transform,
+          display: unfilled.style.display,
+        };
+        stylesArray.push(styles);
+      });
+
+      filled.forEach((filled) => {
+        filled.style.transition = ".5s";
+        const styles = {
+          transition: filled.style.transition,
+          transform: filled.style.transform,
+          display: filled.style.display,
+        };
+        stylesArray.push(styles);
+      });
+
+      // Serialize the styles array to JSON and store it in local storage
+      localStorage.setItem("elementStyles", JSON.stringify(stylesArray));
+    }
+
+    // Function to load styles from local storage
+    function loadStylesFromLocalStorage() {
+      // Retrieve the serialized styles array from local storage
+      const serializedStyles = localStorage.getItem("elementStyles");
+
+      if (serializedStyles) {
+        const stylesArray = JSON.parse(serializedStyles);
+        unfilled.forEach((unfilled) => {
+          const styles = stylesArray.shift();
+          if (styles) {
+            unfilled.style.transition = styles.transition;
+            unfilled.style.transform = styles.transform;
+            unfilled.style.display = styles.display;
+          }
+        });
+
+        filled.forEach((filled) => {
+          const styles = stylesArray.shift();
+          if (styles) {
+            filled.style.transition = styles.transition;
+            filled.style.transform = styles.transform;
+            filled.style.display = styles.display;
+          }
+        });
+      }
+    }
+
+    // Add click event listeners to the elements
+    unfilled.forEach((unfilled, index) => {
+      unfilled.style.transition = "1s";
+      unfilled.addEventListener("click", () => {
+        // Your existing code to modify the element's style
+        unfilled.style.transform = "scale(2.5) rotate(360deg)";
+        setTimeout(() => {
+          unfilled.style.display = "none";
+          filled[index].style.display = "block";
+          filled[index].style.transform = "scale(2.5)";
+          setTimeout(() => {
+            filled[index].style.transform = "scale(1)";
+            unfilled.style.transform = "scale(1)";
+            // Save the updated styles to local storage
+            saveStylesToLocalStorage();
+          }, 500);
+        }, 500);
+      });
+    });
+
+    filled.forEach((filled, index) => {
+      filled.style.transition = "1s";
+      filled.addEventListener("click", () => {
+        // Your existing code to modify the element's style
+        filled.style.transform = "scale(2.5) rotate(360deg)";
+        setTimeout(() => {
+          filled.style.display = "none";
+          unfilled[index].style.display = "block";
+          unfilled[index].style.transform = "scale(2.5)";
+          setTimeout(() => {
+            unfilled[index].style.transform = "scale(1)";
+            filled.style.transform = "scale(1)";
+            // Save the updated styles to local storage
+            saveStylesToLocalStorage();
+          }, 500);
+        }, 500);
+      });
+    });
+
+    // Load the saved styles from local storage when the page loads
+    loadStylesFromLocalStorage();
+
+    //end
+  });
 }
 
 export { getItems };
--- js/navigation.js
+++ js/navigation.js
@@ -2,7 +2,9 @@ const nav = document.querySelector("#mySidenavMobile");
 const navA = document.querySelectorAll(".sidenav a");
 nav.style.transition = "1s";
 
-document.querySelector(".nav_mobile .fa-bars").addEventListener('click', () => {
+document.querySelector(".nav_mobile .fa-bars").addEventListener(
+  "click",
+  () => {
     nav.style.width = "100%";
     nav.style.height = "100%";
     nav.style.margin = "0px";
@@ -13,51 +15,60 @@ document.querySelector(".nav_mobile .fa-bars").addEventListener('click', () => {
     nav.style.transition = "1s";
 
     setTimeout(() => {
-        // nav.style.top = "200px"
-        document.querySelector(".nav_mobile .fa-bars").style.display="none";
-        document.querySelector(".nav_mobile .closing").style.display="block";
-        navA.forEach((navA) => {
-            navA.style.color = "black";
-            navA.style.transition = "1s";
-        })
-    }, 500);
-}, 1000);
-
-document.querySelector(".nav_mobile .closing").addEventListener('click', () => {
-    document.querySelector(".nav_mobile .fa-bars").style.display="block";
-    document.querySelector(".nav_mobile .closing").style.display="none";
-    nav.style.display = "none";
-    navA.forEach((navA) => {
-        navA.style.color = "orange";
+      // nav.style.top = "200px"
+      document.querySelector(".nav_mobile .fa-bars").style.display = "none";
+      document.querySelector(".nav_mobile .closing").style.display = "block";
+      navA.forEach((navA) => {
+        navA.style.color = "black";
         navA.style.transition = "1s";
-    })
-});
+      });
+    }, 500);
+  },
+  1000
+);
 
+document.querySelector(".nav_mobile .closing").addEventListener("click", () => {
+  document.querySelector(".nav_mobile .fa-bars").style.display = "block";
+  document.querySelector(".nav_mobile .closing").style.display = "none";
+  nav.style.display = "none";
+  navA.forEach((navA) => {
+    navA.style.color = "orange";
+    navA.style.transition = "1s";
+  });
+});
 
 //Scroll
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() { myFunction() };
+window.onscroll = function () {
+  myFunction();
+};
 
 // Get the header
 let header = document.getElementById("nav");
-let logo = document.querySelector('.logo');
-let icons = document.querySelector('.icons');
-let cartCounter = document.querySelector('.fa #cart-count');
+let logo = document.querySelector(".logo");
+let icons = document.querySelector(".icons");
+let cartCounter = document.querySelector(".fa #cart-count");
+let sideNav = document.querySelector("#sideNav");
+let uniCategory = document.querySelector("#category");
 
 // Get the offset position of the navbar
 let sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-        logo.classList.add("makeWhite");
-        icons.classList.add("newIcons");
-        cartCounter.classList.add("cartNumber");
-    } else {
-        header.classList.remove("sticky");
-        logo.classList.remove("makeWhite");
-        icons.classList.remove("newIcons");
-        cartCounter.classList.remove("cartNumber");
-    }
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    logo.classList.add("makeWhite");
+    icons.classList.add("newIcons");
+    cartCounter.classList.add("cartNumber");
+    sideNav.classList.add("fixing");
+    uniCategory.classList.add("LeftMargin");
+  } else {
+    header.classList.remove("sticky");
+    logo.classList.remove("makeWhite");
+    icons.classList.remove("newIcons");
+    cartCounter.classList.remove("cartNumber");
+    sideNav.classList.remove("fixing");
+    uniCategory.classList.remove("LeftMargin");
+  }
 }
