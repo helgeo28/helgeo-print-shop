9d28d17a054b8a3b94734fa8ae28aa2ccfa97ad4
josephmalete1997 <josephmalete75@gmail.com>
2023-10-23 14:50:36 +0200

    Making updates

--- js/home.js
+++ js/home.js
@@ -31,3 +31,86 @@ function changeImages() {
   dot();
 }
 setInterval(changeImages, 2500);
+
+//horizontal scrolling
+
+function items_list() {
+  const itemsArray = [
+    "printing/GZ1.jpg",
+    "printing/GZ7.jpg",
+    "printing/GZ8.jpg",
+    "printing/SP1.jpg",
+    "printing/SP2.jpg",
+    "printing/SP3.jpg",
+    "printing/HD1.jpg",
+    "printing/HD2.jpg",
+    "printing/HD3.jpg",
+    "printing/SU1.jpg",
+    "printing/SU1B.jpg",
+    "printing/SU2.jpg",
+    "printing/JS1.jpg",
+    "printing/JS2.jpg",
+    "printing/JS3.jpg",
+    "printing/em1.jpg",
+    "printing/em2.jpg",
+    "printing/em3.jpg",
+    "printing/wk1.jpg",
+    "printing/wk2.jpg",
+    "printing/wk3.jpg",
+    "printing/sb1.webp",
+    "printing/sb2.png",
+    "printing/sb3.webp",
+    "printing/vn1.png",
+    "printing/vn2.jpg",
+    "printing/vn3.jpg",
+  ];
+  const horizontalScroller = document.querySelector(".horizontal-scrolling");
+  horizontalScroller.style.transition = "1s";
+  horizontalScroller.style.position = "absolute";
+  horizontalScroller.style.display = "flex";
+  horizontalScroller.style.overflowX = "auto"; // Add overflowX property for horizontal scrolling
+  horizontalScroller.style.width = `${itemsArray.length * 300}px`; // Ensure the container expands to the available width
+
+  for (let i = 0; i < itemsArray.length; i++) {
+    const items = document.createElement("div");
+    items.style.width = "300px";
+    items.style.height = "300px";
+    items.style.marginRight = "10px";
+    items.style.background = `orange`;
+    items.style.backgroundImage = `url(${itemsArray[i]})`;
+    items.style.backgroundSize = `cover`;
+    items.style.backgroundPosition = `center`;
+    items.style.backgroundRepeat = `no-repeat`;
+    items.style.display = "inline-block"; // Display items in a row
+
+    horizontalScroller.appendChild(items);
+  }
+  counting = 0;
+  document.querySelector("#counter_no").innerHTML = counting;
+  document.querySelector(".l").addEventListener("click", () => {
+    counting -= 300;
+    horizontalScroller.style.left = `${counting}px`;
+    if (counting < -6600) {
+      counting = -6600;
+      horizontalScroller.style.left = `${counting}px`;
+    }
+    if (window.innerWidth <= 600) {
+      if (counting < -7500) {
+        counting = -7500;
+        horizontalScroller.style.left = `${counting}px`;
+      }
+    }
+    // document.querySelector("#counter_no").innerHTML = counting;
+  });
+  document.querySelector(".r").addEventListener("click", () => {
+    counting += 300;
+    horizontalScroller.style.left = `${counting}px`;
+    if (counting >= 0) {
+      counting = 0;
+      horizontalScroller.style.left = `${counting}px`;
+    }
+    // document.querySelector("#counter_no").innerHTML = counting;
+  });
+}
+
+items_list();
--- js/text.js
+++ js/text.js
@@ -6,19 +6,29 @@ const arrow = document.querySelectorAll(".text-title .fa");
 //No display
 
 textDetails.forEach((textDetails, index) => {
-    textDetails.addEventListener('click', () => {
-        innerText[index].style.display = "block";
-        textDetails.style.display = "none";
-        textDIsplayTwo[index].style.display = "block";
-        // textDetails.classList.remove("text-title");
-    })
+  textDetails.addEventListener("click", () => {
+    innerText[index].style.display = "block";
+    textDetails.style.display = "none";
+    textDIsplayTwo[index].style.display = "block";
+    // textDetails.classList.remove("text-title");
+  });
 });
 
 textDIsplayTwo.forEach((textDIsplayTwo, indexNew) => {
-    textDIsplayTwo.addEventListener('click', () => {
-        innerText[indexNew].style.display = "none";
-        textDetails[indexNew].style.display = "block";
-        textDIsplayTwo.style.display = "none";
-        // textDetails.classList.add("text-title");
-    })
+  textDIsplayTwo.addEventListener("click", () => {
+    innerText[indexNew].style.display = "none";
+    textDetails[indexNew].style.display = "block";
+    textDIsplayTwo.style.display = "none";
+    // textDetails.classList.add("text-title");
+  });
 });
+
+//Product items
+const product_panel = document.querySelector(".products");
+
+if (product_panel.childNodes.length > 4) {
+  product_panel.style.marginBottom = "400px";
+}
+if (window.innerWidth <= 600) {
+  product_panel.style.marginBottom = "0px";
+}
