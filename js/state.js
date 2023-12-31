d420fbc862f602e34d928146032c2f83eee3f595
josephmalete1997 <josephmalete75@gmail.com>
2023-12-31 06:46:25 +0200

    Added homepage link to the logo

--- js/state.js
+++ js/state.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll(".sidenav a");
+  const goToHome = document.querySelectorAll(".go-to-home a");
+
+  goToHome.forEach((goTo) => {
+    goTo.addEventListener("click", handleNavLinkClick);
+  });
   //items links
   const viewItemLinks = document.querySelectorAll(".viewItems a");
   //favourites
@@ -25,7 +30,9 @@ document.addEventListener("DOMContentLoaded", function () {
     loaders.style.display = "flex";
     loaders.style.opacity = op;
     loaders.style.zIndex = "186";
-    document.querySelector(".count").innerHTML = `<img src="load.png" width="100%">`;
+    document.querySelector(
+      ".count"
+    ).innerHTML = `<img src="load.png" width="100%">`;
     document.querySelector(".count").style.display = "block";
 
     return loaders;
@@ -55,7 +62,9 @@ document.addEventListener("DOMContentLoaded", function () {
       clearInterval(startStart);
       x.style.display = "none";
       x.style.opacity = "0.8";
-      document.querySelector(".count").innerHTML = `<img src="load.png" width="100%">`;
+      document.querySelector(
+        ".count"
+      ).innerHTML = `<img src="load.png" width="100%">`;
       // refresh page
       // window.location.reload();
       // Store the active section in localStorage
